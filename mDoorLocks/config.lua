0ffbf5309cd25bb8ef2d8a5c30e03ace97433c2a
FlawlessEdits <101374744+FlawlessEdits@users.noreply.github.com>
2024-09-22 20:43:13 -0500

    Add files via upload

---
+++ mDoorLocks/config.lua
@@ -0,0 +1,238 @@
+Config = {
+	doorList = {
+		---------------------
+		-- Sandy Shores PD --
+		---------------------
+		-- cell door for cells
+		[1] = { 
+			["objName"] = "gn_prop_sheriff2_cells_door_right", 
+			["x"] = 1827.56, 
+			["y"] = 3684.55, 
+			["z"] = 29.66, 
+			["locked"] = true, 
+			["distance"] = 2
+		},
+		-- holding cell
+		[2] = { 
+			["objName"] = "gn_prop_sheriff2_cells_door_right", 
+			["x"] = 1824.14, 
+			["y"] = 3687.2, 
+			["z"] = 29.66, 
+			["locked"] = true, 
+			["distance"] = 2
+		},
+		-- cell door for cells 2
+		[3] = { 
+			["objName"] = "gn_prop_sheriff2_cells_door_right", 
+			["x"] = 1821.02, 
+			["y"] = 3692.58, 
+			["z"] = 29.66, 
+			["locked"] = true, 
+			["distance"] = 2
+		},
+		-- Cell 1
+		[4] = { 
+			["objName"] = "gn_prop_sheriff2_cells_door_right", 
+			["x"] = 1817.9, 
+			["y"] = 3692.48, 
+			["z"] = 29.66, 
+			["locked"] = true, 
+			["distance"] = 2
+		},
+		-- Cell 2
+		[5] = { 
+			["objName"] = "gn_prop_sheriff2_cells_door_right", 
+			["x"] = 1814.97, 
+			["y"] = 3690.87, 
+			["z"] = 29.66, 
+			["locked"] = true, 
+			["distance"] = 2
+		},
+		-- Cell 3
+		[6] = { 
+			["objName"] = "gn_prop_sheriff2_cells_door_right", 
+			["x"] = 1816.43, 
+			["y"] = 3688.04, 
+			["z"] = 29.66, 
+			["locked"] = true, 
+			["distance"] = 2
+		},
+		-- Cell 4
+		[7] = { 
+			["objName"] = "gn_prop_sheriff2_cells_door_right", 
+			["x"] = 1812.09, 
+			["y"] = 3689.17, 
+			["z"] = 29.66, 
+			["locked"] = true, 
+			["distance"] = 2
+		},
+		-- Cell 5
+		[8] = { 
+			["objName"] = "gn_prop_sheriff2_cells_door_right", 
+			["x"] = 1809.25, 
+			["y"] = 3687.46, 
+			["z"] = 29.66, 
+			["locked"] = true, 
+			["distance"] = 2
+		},
+		-- Cell 6
+		[9] = { 
+			["objName"] = "gn_prop_sheriff2_cells_door_right", 
+			["x"] = 1806.27, 
+			["y"] = 3685.77, 
+			["z"] = 29.66, 
+			["locked"] = true, 
+			["distance"] = 2
+		},
+		-- Cell 7
+		[9] = { 
+			["objName"] = "gn_prop_sheriff2_cells_door_right", 
+			["x"] = 1808.41, 
+			["y"] = 3683.42, 
+			["z"] = 29.66, 
+			["locked"] = true, 
+			["distance"] = 2
+		},
+		-- Cell 8
+		[9] = { 
+			["objName"] = "gn_prop_sheriff2_cells_door_right", 
+			["x"] = 1803.25, 
+			["y"] = 3684.08, 
+			["z"] = 29.66, 
+			["locked"] = true, 
+			["distance"] = 2
+		},
+		-- Cell 9
+		[9] = { 
+			["objName"] = "gn_prop_sheriff2_cells_door_right", 
+			["x"] = 1805.45, 
+			["y"] = 3681.73, 
+			["z"] = 29.66, 
+			["locked"] = true, 
+			["distance"] = 2
+		},
+		-- Cell 9
+		[9] = { 
+			["objName"] = "gn_prop_sheriff2_cells_door_right", 
+			["x"] = 1803.34, 
+			["y"] = 3681.76, 
+			["z"] = 29.66, 
+			["locked"] = true, 
+			["distance"] = 2
+		},
+		-- Cell door back to exit
+		[9] = { 
+			["objName"] = "gn_prop_sheriff2_cells_door_right", 
+			["x"] = 1812.01, 
+			["y"] = 3683.63, 
+			["z"] = 29.66, 
+			["locked"] = true, 
+			["distance"] = 2
+		},
+		-----------------------------
+		-- DOORS FOR UNION ROAD PD --
+		-----------------------------
+		-- interro 1
+		[9] = { 
+			["objName"] = "gn_prop_sheriff2_cells_door_right", 
+			["x"] = 2809.61, 
+			["y"] = 4750.79, 
+			["z"] = 43.53, 
+			["locked"] = true, 
+			["distance"] = 2
+		},
+		-- interro 2
+		[9] = { 
+			["objName"] = "gn_prop_sheriff2_cells_door_right", 
+			["x"] = 2811.79, 
+			["y"] = 4751.18, 
+			["z"] = 43.53, 
+			["locked"] = true, 
+			["distance"] = 2
+		},
+		-- holding cell
+		[9] = { 
+			["objName"] = "gn_prop_sheriff2_cells_door_right", 
+			["x"] = 2809.28, 
+			["y"] = 4733.43, 
+			["z"] = 43.53, 
+			["locked"] = true, 
+			["distance"] = 2
+		},
+		-- Cell 1
+		[9] = { 
+			["objName"] = "gn_prop_sheriff2_cells_door_right", 
+			["x"] = 2816.85, 
+			["y"] = 4736.19, 
+			["z"] = 43.53, 
+			["locked"] = true, 
+			["distance"] = 2
+		},
+		-- Cell 2
+		[9] = { 
+			["objName"] = "gn_prop_sheriff2_cells_door_right", 
+			["x"] = 2817.48, 
+			["y"] = 4733.44, 
+			["z"] = 43.53, 
+			["locked"] = true, 
+			["distance"] = 2
+		},
+		-- Cell 3
+		[9] = { 
+			["objName"] = "gn_prop_sheriff2_cells_door_right", 
+			["x"] = 2818.1, 
+			["y"] = 4730.75, 
+			["z"] = 43.53, 
+			["locked"] = true, 
+			["distance"] = 2
+		},
+		-- Cell 4
+		[9] = { 
+			["objName"] = "gn_prop_sheriff2_cells_door_right", 
+			["x"] = 2816.68, 
+			["y"] = 4729.96, 
+			["z"] = 43.53, 
+			["locked"] = true, 
+			["distance"] = 2
+		},
+		-- Cell 5
+		[9] = { 
+			["objName"] = "gn_prop_sheriff2_cells_door_right", 
+			["x"] = 2813.33, 
+			["y"] = 4729.18, 
+			["z"] = 43.53, 
+			["locked"] = true, 
+			["distance"] = 2
+		},
+		----------------------
+		--LSPD Head Quarters--
+		----------------------
+		-- interro 98
+		[9] = { 
+			["objName"] = "gn_prop_sheriff2_cells_door_right", 
+			["x"] = -587.97, 
+			["y"] = -448.71, 
+			["z"] = 31.16, 
+			["locked"] = true, 
+			["distance"] = 2
+		},
+		-- interro 98 
+		[9] = { 
+			["objName"] = "gn_prop_sheriff2_cells_door_right", 
+			["x"] = -575.14, 
+			["y"] = -448.75, 
+			["z"] = 31.16, 
+			["locked"] = true, 
+			["distance"] = 2
+		},
+		-- interro 98
+		[9] = { 
+			["objName"] = "gn_prop_sheriff2_cells_door_right", 
+			["x"] = -587.97, 
+			["y"] = -448.71, 
+			["z"] = 31.16, 
+			["locked"] = true, 
+			["distance"] = 2
+		},
+	}
+}
