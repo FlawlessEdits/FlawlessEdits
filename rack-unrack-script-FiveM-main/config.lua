c979eaa4c91885b6f0a05c438288b1df0a66b5f4
FlawlessEdits <101374744+FlawlessEdits@users.noreply.github.com>
2024-09-22 20:50:17 -0500

    Add files via upload

---
+++ rack-unrack-script-FiveM-main/config.lua
@@ -0,0 +1,23 @@
+-----------------------------------------------------------
+----          Police Rack and Unrack script            ----
+----             Made by Tamir112#5345                 ----
+----        For more scripts and updates Join:         ----
+----              discord.gg/bDPaRDeBKJ                ----
+----                                                   ----
+-----------------------------------------------------------
+
+
+----              To find the weapon hashs use:              ----
+---- https://wiki.gtanet.work/index.php?title=Weapons_Models ----
+
+
+----------- Configure the script to your liking below -----------
+config 				  = {}
+
+-- First weapon used with the rack/unrack command --
+config.weapon1 = 4208062921-- weapon hash, default set to carbine rifle.
+config.weapon1name = "AR-15" --Name of the gun being Unracked.
+
+-- second weapon used with the rack1/unrack1 command --
+config.weapon2 = 1432025498 --weapon hash, default set to pump shotgun.
+config.weapon2name = "Pump Shotgun" --Name of the gin being Unracked.
